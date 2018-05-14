ce3e90cd3100e29c9785cfc1626ddd0ff71fd1ce
xuli <78645574@qq.com>
2018-05-14 17:03:00 +0800

    格式化

--- sourcecode/learnssm-firstssm/src/main/java/com/iot/learnssm/firstssm/interceptor/HandlerInterceptor1.java
+++ sourcecode/learnssm-firstssm/src/main/java/com/iot/learnssm/firstssm/interceptor/HandlerInterceptor1.java
@@ -10,9 +10,18 @@ import javax.servlet.http.HttpServletResponse;
  * Created by brian on 2016/3/7.
  */
 public class HandlerInterceptor1 implements HandlerInterceptor {
-    //进入 Handler方法之前执行
-    //用于身份认证、身份授权
-    //比如身份认证，如果认证通过表示当前用户没有登陆，需要此方法拦截不再向下执行
+    /**
+     * 进入 Handler方法之前执行
+     * 用于身份认证、身份授权
+     * 比如身份认证，如果认证通过表示当前用户没有登陆，需要此方法拦截不再向下执行
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("HandlerInterceptor1...preHandle");
         //return false表示拦截，不向下执行
@@ -20,14 +29,32 @@ public class HandlerInterceptor1 implements HandlerInterceptor {
         return true;
     }
 
-    //进入Handler方法之后，返回modelAndView之前执行
-    //应用场景从modelAndView出发：将公用的模型数据(比如菜单导航)在这里传到视图，也可以在这里统一指定视图
+    /**
+     * 进入Handler方法之后，返回modelAndView之前执行
+     * 应用场景从modelAndView出发：将公用的模型数据(比如菜单导航)在这里传到视图，也可以在这里统一指定视图
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         System.out.println("HandlerInterceptor1...postHandle");
     }
 
-    //执行Handler完成执行此方法
-    //应用场景：统一异常处理，统一日志处理
+    /**
+     * 执行Handler完成执行此方法
+     * 应用场景：统一异常处理，统一日志处理
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         System.out.println("HandlerInterceptor1...afterCompletion");
     }
